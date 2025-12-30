6d5e1d875e3b7d14ddcedaf72a7925539b4b533d
Praveen <praveenk@rpsconsulting.in>
2025-12-30 12:05:44 +0530

    usecase on hashset

---
+++ JAVA/Programs/corejava/src/collections/usecases/EmmaHashSetDemo.java
@@ -0,0 +1,40 @@
+package collections.usecases;
+/*
+ * Emma is creating a small Java program to simulate a dice game. 
+ * She wants to keep track of the distinct numbers that appear 
+ * when the dice is rolled multiple times. the program should store 
+ * only unique dice outcomes.
+
+Requirements:
+
+Create a class named DiceGame with the following:
+
+A private HashSet<Integer> called diceValues to store unique dice 
+roll results.
+
+A constructor that initializes the diceValues HashSet.
+
+A method recordDiceValue(int value) that adds a dice roll value to the 
+HashSet.
+
+A method getUniqueDiceValues() that returns all the unique dice values 
+stored.
+
+In the Main class:
+
+Instantiate the DiceGame class as diceGame.
+
+Use the Scanner class to read the total number of dice rolls from the user.
+
+Accept the value of each dice roll in a loop and store it using the recordDiceValue method.
+
+Print all unique dice values using the getUniqueDiceValues method.
+ */
+public class EmmaHashSetDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
