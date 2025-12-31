bc3ec9b808cfe6a84ab1b813a241b92bdd1f65c0
Praveen <praveenk@rpsconsulting.in>
2025-12-31 17:08:18 +0530

    added other techniques

--- JAVA/Programs/datastructures/src/datastructures/SortTechniques.java
+++ JAVA/Programs/datastructures/src/datastructures/SortTechniques.java
@@ -2,6 +2,36 @@ package datastructures;
 
 public class SortTechniques {
 	
+	public int[] quickSort(int[] a, int low, int high) {
+		if (low < high) {
+			int pi = partition(a, low, high);
+			quickSort(a, low, pi - 1);
+			quickSort(a, pi + 1, high);
+		}
+		return a;
+	}
+	
+	public int partition(int[] a, int low, int high) {
+		int pivot = a[high];
+		int i = (low - 1);
+		for (int j = low; j < high; j++) {
+			if (a[j] < pivot) {
+				i++;
+
+				int temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+
+		int temp = a[i + 1];
+		a[i + 1] = a[high];
+		a[high] = temp;
+
+		return i + 1;
+	}
+	
+	
 	public int[] bubbleSort(int[] a) {
 		int n = a.length;
 		for (int i = 0; i < n - 1; i++) {
--- JAVA/Programs/datastructures/src/datastructures/TestSortingTecniques.java
+++ JAVA/Programs/datastructures/src/datastructures/TestSortingTecniques.java
@@ -6,14 +6,15 @@ public class TestSortingTecniques {
 		// TODO Auto-generated method stub
 		
 		SortTechniques st = new SortTechniques();
-		int a[] = {64, 34, 25, 12, 22, 11, 90};
+		int a[] = {64, 34, 25, 12, 22, 11, 90, 31};
 		System.out.println("Unsorted array");
 		for (int i = 0; i < a.length; i++) {
 			System.out.print(a[i] + " ");
 		}
 		System.out.println();
+		int sortedArray[] = st.quickSort(a, 0, a.length - 1);
 		//int sortedArray[] = st.bubbleSort(a);
-		int sortedArray[] = st.mergeSort(a);
+		//int sortedArray[] = st.mergeSort(a);
 		System.out.println("Sorted array");
 		for (int i = 0; i < sortedArray.length; i++) {
 			System.out.print(sortedArray[i] + " ");
